6f3e9fce15b62727447ed17686630ebfa34af6f5
Manuela Cardenas <manuelacardenasj@gmail.com>
2018-07-31 13:09:31 -0400

    Update Markets with fucntion that renders market addresses

---
+++ client/src/components/Market.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+//
+// export default class Market extends React.Component{
+//
+//   render(){
+//     return(
+//       <div>
+//         <ul >
+//           <li> {this.props.market.marketdetails.Address}</li>
+//           <li> {this.props.market.marketdetails.GoogleLink}</li>
+//           <li> {this.props.market.marketdetails.Products}</li>
+//           <li> {this.props.market.marketdetails.Schedule}</li>
+//         </ul>
+//       </div>
+//     )
+//   }
+// }
