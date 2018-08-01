8c0219ada65884778f9b4a9c6201aa3ee22a68d4
Manuela Cardenas <manuelacardenasj@gmail.com>
2018-08-01 11:51:01 -0400

    Final changes

--- client/src/components/Market.js
+++ client/src/components/Market.js
@@ -4,12 +4,14 @@ export default class Market extends React.Component{
 
   render(){
     return(
-      <div id="Markets">
+      <div id="market-div">
+        <div id="Markets">
         <ul>
           <li> Name: {this.props.market.myMarketName}</li>
           <li> Address: {this.props.market.Address}</li>
           <li> Products: {this.props.market.Products}</li>
         </ul>
+        </div>
       </div>
     )
   }
--- client/src/components/MarketForm.js
+++ client/src/components/MarketForm.js
@@ -1,47 +1,73 @@
 import React, { Component } from 'react';
+import {Input, Menu} from 'semantic-ui-react';
 import {Link} from 'react-router-dom';
+
+const navItem = {
+ width: '125px',
+ padding: 'auto',
+ fontSize: '1.5em',
+}
+const space ={
+  marginRight: '3rem',
+}
+
+const form = {
+ display: 'flex',
+ flexDirection: 'row',
+ margin: 'auto',
+}
+
+const header = {
+ fontSize: '1.5em',
+}
+
+const sub = {
+ fontSize: '2em',
+ align: 'center',
+ marginTop: '10px',
+ paddingTop: '10px'
+}
 
 
 class MarketForm extends React.Component {
-   constructor() {
-     super();
-     this.handleSubmit = this.handleSubmit.bind(this);
-   }
+ constructor() {
+   super();
+   this.handleSubmit = this.handleSubmit.bind(this);
+ }
 
-   async handleSubmit(e) {
-     e.preventDefault();
-     await this.props.getMarkets(this.refs.return.value);
-     this.props.history.push("/Markets")
-   }
+ async handleSubmit(e) {
+   e.preventDefault();
+   await this.props.getMarkets(this.refs.return.value);
+   this.props.history.push("/Markets")
+ }
 
-   render()  {
-       return (
-      <div>
-         <ul id='HeaderId'>
-           <li>
-             <Link to = '/Home'>Home</Link>
-           </li>
-           <li>
-             <Link to = '/About'>About</Link>
-             </li>
-             <li>
-             <Link to = '/Markets'>Your Markets</Link>
-           </li>
-           </ul>
-           <div id="Bar">
-           <form action="" className="farmer-market-form" onSubmit={this.handleSubmit}>
-           <h1 className="form-title">Find your local Farmers Market!</h1>
-          <div>
-             <label htmlFor="city">Enter Zip Code</label>
-             <input type="number" ref='return' name="zipcode"  placeholder="ex: 33018" />
-           </div>
-           <footer>
-             <input type="submit" value="Find my market!" />
-           </footer>
-           </form>
-           </div>
-        </div>
-       );
-   }
+ render()  {
+     return (
+       <Menu id='HeaderId'>
+         <Menu.Item style={navItem}>
+           <Link to = '/Home'>Home</Link>
+         </Menu.Item>
+         <Menu.Item style={navItem}>
+           <Link to = '/About'>About</Link>
+           </Menu.Item>
+           <Menu.Item style={navItem}>
+           <Link to = '/Markets'>Your Markets</Link>
+         </Menu.Item>
+
+         <Menu.Menu style={space} position='right' id="Bar" >
+         <form action="" className="farmer-market-form" onSubmit={this.handleSubmit} style={form}>
+        <div>
+           <label htmlFor="city" style={sub }>Enter Zip Code </label>
+           <input type="number" ref='return' name="zipcode"  placeholder="ex: 33018" />
+         </div>
+         <footer>
+           <input type="submit" value="Find my market!" />
+         </footer>
+         </form>
+         </Menu.Menu>
+     </Menu>
+
+     );
+ }
 }
 export default MarketForm;
